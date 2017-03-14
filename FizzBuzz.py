652ef5b2a0dbefc2b81a3124a1e9718b857dcd85
GafasAV <gafasav@gmail.com>
2017-03-14 17:23:53 +0200

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,19 @@
+def fizzBuzz(n):
+	
+	if n % 3 == 0 and n % 5 == 0:
+		return "FizzBuzz"
+	elif n % 3 == 0:
+		return "Fizz"
+	elif n % 5 == 0:
+		return "Buzz"
+	else:
+		return str(n)
+		
+	
+	
+if __name__ == "__main__":
+	
+	count = int(input("Input numbers count :"))
+	result = ""
+	
+	print(result.join(fizzBuzz(n) for n in range(1, count+1)))
