112986e762ae9d5e5f1c1810acae93c81814cc7a
GafasAV <gafasav@gmail.com>
2017-03-14 17:21:37 +0200

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def fact(n):
+	if n == 0 or n == 1:
+		return 1
+	else:
+		return n * fact(n - 1)
+
+if __name__ == "__main__":
+	
+	count = int(input("Input iteration count :"))
+	
+	result = fc.fact(count)
+	print(result)
