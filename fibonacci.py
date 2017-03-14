69adc3f186609c2953a3ccdf380918a6f5bdbeff
GafasAV <gafasav@gmail.com>
2017-03-14 22:33:43 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
 	n = int(input("Input fibonacci range :"))
 	
 	for i in range(1, n+1):
-		print("{0} step = {1}".format(i, fb.fib(i)))
+		print("{0} step = {1}".format(i, fib(i)))
